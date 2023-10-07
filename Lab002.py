1fa382e66255420fb0261fd8c1eb9eea48c0b198
SnehaPythonAutomation <147255108+SnehaPythonAutomation@users.noreply.github.com>
2023-10-07 19:55:52 +0530

    Add files via upload

---
+++ Lab002.py
@@ -0,0 +1,13 @@
+
+# 1
+name=input("Print the name of the User")
+print(name)
+#2
+name1=input("First Name")
+print(name1)
+name2=input("Last Name")
+print(name2)
+print(name1,name2,sep="_",end='\t')
+#3
+name3=input("User Name")
+print("You are welcome.",name3,"to the Python Class.")

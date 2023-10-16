73923013d5033115b6999c11ae198cbe15335a76
SnehaPythonAutomation <147255108+SnehaPythonAutomation@users.noreply.github.com>
2023-10-16 16:43:29 +0530

    16OctTask2.py

--- 16OctTask2.py
+++ 16OctTask2.py
@@ -6,7 +6,7 @@
 # Output = Leap year
 
 year=int(input("Enter the Year"))
-if year / 100 != 400:
+if (year % 4==0 and year % 100 != 0) or year %  400 ==0:
     print("Leap Year")
 else:
     print("Not Leap Year")
